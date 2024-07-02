a82df9eb7abad733f4ced2564794ba5dc9749548
AntiLiss <magalam000@gmail.com>
2024-07-02 13:41:27 +0300

    Working payment model created

--- app/order/models.py
+++ app/order/models.py
@@ -1,60 +1,82 @@
-# from django.db import models
-# from django.contrib.auth import get_user_model
-# from django.core.validators import MinValueValidator
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 
 
-# class Order(models.Model):
-#     """Order model"""
+class Order(models.Model):
+    """Order model"""
 
-#     user = models.OneToOneField(
-#         to=get_user_model(),
-#         on_delete=models.CASCADE,
-#         primary_key=True,
-#     )
-#     total = models.DecimalField(
-#         max_digits=15,
-#         decimal_places=2,
-#         validators=[MinValueValidator(0)],
-#         default=0,
-#     )
-#     shipping_address = models.ForeignKey(
-#         to="user.ShippingAddress",
-#         on_delete=models.SET_NULL,
-#         null=True,
-#     )
-#     payment = models.ForeignKey(
-#         to=Payment,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#     )
+    user = models.OneToOneField(
+        to=get_user_model(), on_delete=models.CASCADE, primary_key=True
+    )
+    shipping_address = models.ForeignKey(
+        to="user.ShippingAddress",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    payment = models.ForeignKey(
+        to="Payment",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    total = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+        default=0,
+    )
 
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
-# class OrderItem(models.Model):
-#     """Cart item model"""
+class OrderItem(models.Model):
+    """Cart item model"""
 
-#     order = models.ForeignKey(
-#         to=Order, on_delete=models.CASCADE, related_name="order_items"
-#     )
-#     product = models.ForeignKey(to="product.Product", on_delete=models.CASCADE)
-#     quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    order = models.ForeignKey(
+        to=Order, on_delete=models.CASCADE, related_name="order_items"
+    )
+    product = models.ForeignKey(to="product.Product", on_delete=models.CASCADE)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
 
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     def get_total_cost(self):
-#         """
-#         Get the total cost of the order item taking into account
-#         its quantity and discount.
-#         """
-#         return round(self.product.calculate_final_price() * self.quantity, 2)
+    def get_total_cost(self):
+        """
+        Get the total cost of the order item taking into account
+        its quantity and discount.
+        """
+        return round(self.product.calculate_final_price() * self.quantity, 2)
 
-#     class Meta:
-#         constraints = [
-#             # Restrict adding the same product to order again
-#             models.UniqueConstraint(
-#                 fields=["order", "product"], name="unique_order_product"
-#             )
-#         ]
+    class Meta:
+        constraints = [
+            # Restrict adding the same product to order again
+            models.UniqueConstraint(
+                fields=["order", "product"], name="unique_order_product"
+            )
+        ]
+
+
+class Payment(models.Model):
+    """Payment model"""
+
+    # Payment status choices
+    PENDING = "P"
+    COMPLETED = "C"
+    FAILED = "F"
+    STATUS_CHOICES = (
+        (PENDING, _("pending")),
+        (COMPLETED, _("completed")),
+        (FAILED, _("failed")),
+    )
+
+    # Payment provider choices
+    STRIPE = "S"
+    PROVIDER_CHOICES = ((STRIPE, "stripe"),)
+
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    provider = models.CharField(max_length=1, choices=PROVIDER_CHOICES)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
